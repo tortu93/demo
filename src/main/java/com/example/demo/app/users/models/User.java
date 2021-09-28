cba523fa06f3b040cb138d9fbf63d88446577528
Arturo Muela <eltorturo93@gmail.com>
2021-09-27 19:16:23 -0600

    first commit

---
+++ src/main/java/com/example/demo/app/users/models/User.java
@@ -0,0 +1,56 @@
+package com.example.demo.app.users.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="adm_user")
+public class User {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name="user_seq_gen", sequenceName = "user_id_seq", allocationSize = 1)
+    private Integer id;
+    @Column(name="user_name", columnDefinition = "varchar(40)", nullable = false, unique = true)
+    private String userName;
+    @Column(name="email", columnDefinition = "varchar(40)",nullable = false,unique = false)
+    private String eMail;
+    @Column(name="phone", columnDefinition = "varchar(20)")
+    private String phone;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/example/demo/app/users/restcontrollers/UserRestController.java
@@ -0,0 +1,36 @@
+package com.example.demo.app.users.restcontrollers;
+
+import com.example.demo.app.users.models.User;
+import com.example.demo.app.users.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public Integer findById(@PathVariable("id")Integer id){
+        return id;
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    @GetMapping
+    public List<User>findAll(){
+        return userService.findAll();
+    }
+
+
+}
+
